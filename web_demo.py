ae341fb71c9d8f1266bd7a3533a70cb2f6a3e99e
cardiffle <840957982@qq.com>
2024-03-08 11:36:58 +0800

    add vectordb and data load module

--- web_demo.py
+++ web_demo.py
@@ -7,18 +7,20 @@ from vectordb_utils import InMemoryVecDB
 from pdf_utils import extract_text_from_pdf
 from text_utils import split_text
 from rerank_utils import rerank
+from vectordb_utils_shule import ShuleVectorDB
 from sentence_transformers import CrossEncoder
 from dotenv import load_dotenv, find_dotenv
 import os
+import pandas as pd
 
 vec_db = InMemoryVecDB()
+vec_db_shule = ShuleVectorDB()
 _ = load_dotenv(find_dotenv()) 
-path = os.getenv('RERANK_MODEL_PATH')
-rerank_model = CrossEncoder(path)
+rerank_model = CrossEncoder(os.getenv('RERANK_MODEL_PATH'))
 top_n = 5
 recall_n = 50
 
-def init_db(file):
+def init_db_pdf(file):
     print("---init database---")
     paragraphs = extract_text_from_pdf(file.name)
     # larger intersect
@@ -27,19 +29,51 @@ def init_db(file):
     print(len(documents))
     vec_db.add_documents_bge(documents)
 
+# load data from ./corpus
+def init_db_local():
+    print("---init database begin---")
+    corpus_path = os.getenv('CORPUS_PATH')
+    
+    # paper 
+    print("local database of paper initing...") 
+    df = pd.read_csv("/Users/lishule/Documents/namespace/AMRGPT/main/corpus/sample_8.csv")[["Title", "Year", "DOI", "Abstract"]]
+    for i in range(df.shape[0]):
+        documents = split_text(df["Abstract"][i], 400, 100)
+        vec_db_shule.add_documents_bge(type="paper",
+                                       texts=documents, 
+                                       title=df["Abstract"][i],
+                                       year=df["Year"][i],
+                                       source=df["DOI"][i])
+    
+    # report pdf
+    print("local database of report initing...") 
+    pdf_files = [file for file in os.listdir(corpus_path) if file.endswith('.pdf')]
+    for pdf_file in pdf_files:
+        paragraphs = extract_text_from_pdf(os.path.join(corpus_path, pdf_file))
+        # larger intersect
+        documents = split_text(paragraphs, 400, 100)
+        os.path.splitext(pdf_file)[0].split('_')
+        vec_db_shule.add_documents_bge(type="report",
+                                       texts=documents, 
+                                       title=os.path.splitext(pdf_file)[0].split('_')[3],
+                                       year=os.path.splitext(pdf_file)[0].split('_')[1],
+                                       source=os.path.splitext(pdf_file)[0].split('_')[0])
+    print("---init database finish---")
+    
 
 def chat(user_input, chatbot, context, search_field, search_strategy = "rerank"):
     print("---chat button---")
     search_results = []
     if search_strategy == "base":
-        search_results = vec_db.search_bge(user_input, top_n)
+        search_results = vec_db_shule.search_bge(user_input, top_n)
         search_field = "\n\n".join([f"{index+1}. {item}" for index, item in enumerate(search_results)])
     elif search_strategy == "rerank":
+        print("===rerank===")
         search_results = rerank(user_input, top_n, recall_n)
         search_field = "\n\n".join([f"{index+1}. (score: {item[0]:.2e}), {item[1]}" for index, item in enumerate(search_results)])
 
     print("===building prompt===")
-    prompt = build_prompt(info=search_results, query=user_input)
+    prompt = build_prompt(info=[item[1] for item in search_results], query=user_input)
     print("prompt content built:\n", prompt)
     print("===get completion===")
     response = get_completion_openai(prompt, context)
@@ -49,10 +83,12 @@ def chat(user_input, chatbot, context, search_field, search_strategy = "rerank")
     context.append({'role': 'assistant', 'content': response})
     return "", chatbot, context, search_field
 
-def rerank(user_input, top_n = 5, recall_n = 50):
-    search_results = vec_db.search_bge(user_input, recall_n)
+def rerank(user_input, top_n=5, recall_n=50, verbose=True):
+    search_results = vec_db_shule.search_bge(user_input, recall_n)
     scores = rerank_model.predict([(user_input, doc) for doc in search_results])
     sorted_list = sorted(zip(scores,search_results), key=lambda x: x[0], reverse=True)
+    if verbose:
+        print(f"finish rerank {len(sorted_list)} texts, return highest {top_n} texts")
     # for score, doc in sorted_list:
     #     print(f"{score}\t{doc}\n")
     return sorted_list[:top_n]
@@ -91,10 +127,11 @@ def main():
                         [user_input, chatbot, context, search_field])
         emptyBtn.click(reset_state, outputs=[chatbot, context, user_input, search_field])
 
-        fileCtrl.upload(init_db, inputs=[fileCtrl])
+        # fileCtrl.upload(init_db_pdf, inputs=[fileCtrl])
 
     demo.queue().launch(share=False, server_name='0.0.0.0', server_port=8889, inbrowser=True)
 
 
 if __name__ == "__main__":
+    init_db_local()
     main()
